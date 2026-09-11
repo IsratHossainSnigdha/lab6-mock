b7a3d8399bb55d4f103fe547af112048853999f5
IsratHossainSnigdha <israthossainsnigdha@gmail.com>
2026-09-11 23:38:48 +0600

    Initial Lab 6 application

---
+++ server.js
@@ -0,0 +1,23 @@
+const http = require("http");
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/html" });
+
+    res.end(`
+        <html>
+            <head>
+                <title>Lab 6 Mock Exam</title>
+            </head>
+            <body>
+                <h1>Lab 6 Deployment Successful! 🎉</h1>
+                <p>Deployed using GitHub Actions → VPS → Nginx.</p>
+            </body>
+        </html>
+    `);
+});
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
