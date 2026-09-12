f5ae8980dfb5706467ea6580020fc9c10520faee
IsratHossainSnigdha <israthossainsnigdha@gmail.com>
2026-09-12 11:07:36 +0600

    Test automatic deployment

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const server = http.createServer((req, res) => {
                 <title>Lab 6 Mock Exam</title>
             </head>
             <body>
-                <h1>Lab 6 Deployment Successful yeeee! 🎉</h1>
+                <h1>Lab 6 Deployment Successful yeeeeeeeeee! 🎉</h1>
                 <p>Deployed using GitHub Actions → VPS → Nginx.</p>
             </body>
         </html>
